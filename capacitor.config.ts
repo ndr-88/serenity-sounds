89afaa41df734ccddb8c8fe5e2148839187a02e6
Andrea Savo <andrea.savo@maggioli.it>
2024-10-02 21:44:58 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Serenity Sounds',
+  webDir: 'www'
+};
+
+export default config;
